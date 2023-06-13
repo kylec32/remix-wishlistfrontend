24e5f3898ea6d3839c080e8a0f1e44fe23410677
Kyle Carter <kylec32@gmail.com>
2023-06-12 22:50:55 -0600

    Some work towards user search

--- app/routes/user-search.ts
+++ app/routes/user-search.ts
@@ -1,12 +1,21 @@
 import { json } from "@remix-run/node";
-import type { LoaderArgs } from "@remix-run/node";
+import type { LoaderArgs, LoaderFunction } from "@remix-run/node";
+import { badRequest } from "~/utils/request.server";
 
-export async function loader({ params }: LoaderArgs) {
-    return new Response(JSON.stringify({key:'value'}), {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    // return json({key:'value'});
+import { searchForUser } from '~/utils/user-service.server';
+import invariant from "tiny-invariant";
+
+export async function loader({ request }: LoaderArgs) {
+  const searchParams = new URL(request.url).searchParams;
+  const filter = searchParams.get('filter');
+  if (filter !== null) {
+    return new Response(JSON.stringify(await searchForUser(filter)), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } else {
+    throw badRequest({'message': 'no filter found'});
   }
+}
---
+++ app/utils/user-service.server.ts
@@ -0,0 +1,14 @@
+import { db } from '~/utils/db.server';
+import { badRequest } from './request.server';
+
+export async function searchForUser(searchCriteria: string) {
+    return await db.user.findMany({
+        where: {
+            OR: [
+                {first_name: { contains: searchCriteria }},
+                {last_name: { contains: searchCriteria }},
+                {email_address: { contains: searchCriteria }},
+            ]
+        }
+    })
+}
