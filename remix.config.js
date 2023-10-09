7975baafc76033208ece26854a4f39700f2bf49a
Kyle Carter <kylec32@gmail.com>
2023-10-08 19:04:29 -0600

    I believe successfully upgraded

--- remix.config.js
+++ remix.config.js
@@ -7,9 +7,6 @@ module.exports = {
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
   future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
+
   },
 };
